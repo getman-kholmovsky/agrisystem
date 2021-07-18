c94d48a54b91c73f5b2eb15000b63f2e61aee391
Dima Getman <dima.getman12112001@gmail.com>
2021-07-18 06:34:14 +0300

    Init project

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const config = require('config');
+
+const app = express();
+
+const PORT = config.get('port') || 5000;
+
+app.listen(5000, () => {
+    console.log(`App has been started on port ${PORT}...`)
+    console.log(`http://localhost:${PORT}`)
+});
