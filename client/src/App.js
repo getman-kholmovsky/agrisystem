380560305c7b8442c5041796fdcc2e9206d903f0
oleg-kh <OlegKholmovsky@gmail.com>
2021-07-20 00:37:48 +0300

    Добавил react`
    добавил react

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import CardList from './components/CardList';
+import FamilyPicker from './components/FamilyPicker';
+import Pagination from './components/Pagination';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Plants from './components/Plants';
+import PageNotFound from './components/PageNotFound';
 
-function App() {
+const AppWrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 1rem;
+  background: white;
+`;
+
+const HeaderText = styled(Link)`
+  font-size: 4rem;
+  font-weight: 400;
+  text-decoration: none;
+  color: black;
+
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const [totalCount, settotalCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChange = (pageNumber) => {
+    setData(eval(`fakeData${pageNumber}`));
+    setCurrentPage(pageNumber);
+  };
+
+  const numberPages = Math.floor(totalCount / 10 - 1);
+
+  const getPage = async (pageNumber) => {
+    const newData = await fetch('/api/agriculture').then((data) => data.json());
+    const page = newData.page;
+    const pageData = newData.data;
+    return { page, pageData };
+  };
+  useEffect(() => {
+    getPage().then(({ page, pageData }) => {
+      setCurrentPage(page);
+      setData(pageData);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppWrapper>
+        <Header>
+          <HeaderText to='/'>Plants bank</HeaderText>
+        </Header>
+        <Switch>
+          <Route path='/plant/:id'>
+            <Plants />
+          </Route>
+          <Route exact path='/'>
+            <FamilyPicker />
+            {data && <CardList data={data} />}
+            {totalCount > 10 ? (
+              <Pagination
+                pages={numberPages}
+                currentPage={currentPage}
+                handlePageChange={handlePageChange}
+              />
+            ) : (
+              ''
+            )}
+          </Route>
+          <Route path='*'>
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </AppWrapper>
+    </Router>
   );
-}
+};
 
 export default App;
