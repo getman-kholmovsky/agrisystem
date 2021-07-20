e77cc1ee946b8d24d24c1241e91879e5b8f2f24e
Dima Getman <dima.getman12112001@gmail.com>
2021-07-21 02:20:39 +0300

    Реализована возможность загрузки изображения в agriculture

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const config = require('config');
 const db = require('./db');
 
 const app = express();
-
-app.use(express.json());
+app.use(express.json({limit: '50mb', extended: true}));
+app.use('/uploads', express.static('uploads'));
 
 app.use('/auth', require('./routes/auth'));
 app.use('/api', require('./routes/api'));
